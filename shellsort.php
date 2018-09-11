12adca2b2d6b66bb789f414e3d43ca999954e909
zhanzstudy <605178606@qq.com>
2018-09-11 20:55:00 +0800

    希尔排序 直接插入排序

--- shellsort.php
+++ shellsort.php
@@ -1,5 +1,23 @@
 <?php
 
+//直接插入排序
+function insertsort(&$arr, $high)
+{
+    for ($i = 2; $i <= $high; $i++)
+    {
+        if ($arr[$i] < $arr[$i - 1])
+        {
+            $arr[0] = $arr[$i];
+            for ($j = $i - 1; $j > 0 && $arr[$j] > $arr[0]; $j--)
+            {
+                $arr[$j + 1] = $arr[$j];
+            }
+
+            $arr[$j + 1] = $arr[0];
+        }
+    }
+}
+
 //希尔排序
 function shellsort(&$arr, $high)
 {
@@ -39,7 +57,9 @@ $arr = array(
     9 => 7,
 );
 
-shellsort($arr, 9);
+//shellsort($arr, 9);
+
+insertsort($arr, 9);
 
 echo "<pre>";
 print_r($arr);
